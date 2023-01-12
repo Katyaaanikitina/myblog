a878322a71f6e34b294d9269de572021a63c7ddf
Katya <katyanikitina16@gmail.com>
2023-01-12 12:39:36 +0100

    Created edit page

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -32,6 +32,21 @@ export class PostsService {
         }))
     }
 
+    getById(id: string):Observable<Post> {
+        return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`)
+            .pipe(map((post: Post) => {
+                return {
+                    ...post,
+                    id,
+                    date: new Date(post.date)
+                };
+            }));
+    }
+
+    update(post: Post): Observable<Post> {
+        return this.http.patch<Post>(`${environment.fbDbUrl}/posts/${post.id}.json`, post)
+    }
+
     remove(id: string): Observable<void> {
       return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
     }
