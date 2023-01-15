23e2d8b5b913e54a4c6eebe6e096533efb6f3f8f
Katya <katyanikitina16@gmail.com>
2023-01-15 11:53:31 +0100

    Created output of post on the post-page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Post } from '../shared/interfaces';
+import { PostsService } from '../shared/posts.service';
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+post$: Observable<Post>
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+      })
+    )
+     
   }
 
 }
